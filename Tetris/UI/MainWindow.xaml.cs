b6dbc608043274f5b481930ae6c30cefbe81fd72
Phiwokwakhe Khathwane <phiwokwakhe299@gmail.com>
2023-08-24 18:49:01 +0200

    Grid layout

--- Tetris/UI/MainWindow.xaml.cs
+++ Tetris/UI/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,11 +24,29 @@ namespace Tetris
         public MainWindow()
         {
             InitializeComponent();
-        }
+        }//ctor main
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+            //TODO : Rework the resize feature
+        }//Window_SizeChanged
 
-        }
-    }
-}
+        private void btnRestart_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //var cl = new Color() { R = byte.Parse("166"), G = byte.Parse("247"), B = byte.Parse("187") };
+            btnRestart.Background = Brushes.Red;//new SolidColorBrush(Color.FromArgb(0,166, 247, 187));
+        }//btnRestart_MouseEnter
+        private void btnRestart_MouseLeave(object sender, MouseEventArgs e)
+        {
+            //btnRestart.Background = Brushes.Transparent;
+        }//btnRestart_MouseLeave
+        private void btnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+            widthAnimation.From = 160;
+            widthAnimation.To = this.Width - 30;
+            widthAnimation.Duration = new TimeSpan(5);
+            btnRestart.BeginAnimation(Button.WidthProperty, widthAnimation);
+        }//btnRestart_Click
+    }//Class
+}//namespace
